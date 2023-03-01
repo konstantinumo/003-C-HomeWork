90090bee07106cfc125e870b26c5dc9e46d2e418
konstantin <konst.geekbrains@mail.ru>
2023-03-01 18:21:13 +0300

    Ищем максимум

---
+++ 01-FirstLook/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Ищем максимум");
+Console.WriteLine("Введите 1 число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > max) max = b;
+Console.Write("Максимум: ");
+Console.WriteLine(max);
