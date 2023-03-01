d59091d5e118f91348c64ec54ccb3d311388b175
konstantin <konst.geekbrains@mail.ru>
2023-03-01 19:44:23 +0300

    Находим вторую цифру в трехзначном числе

---
+++ 02_ArraysAndFunctions/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int number = new Random().Next(100,500);
+Console.WriteLine(number);
+
+int DecNumber(int num)
+{
+    int tmp = num % 100;
+    tmp = tmp / 10;
+    return tmp;
+}
+
+int a = DecNumber(number);
+Console.WriteLine(a);
