5df046588b332937952e8e46fe231bc905223522
konstantin <konst.geekbrains@mail.ru>
2023-03-01 18:37:16 +0300

    Проверяем на четность

--- 01-FirstLook/Program.cs
+++ 01-FirstLook/Program.cs
@@ -1,9 +1,17 @@
-﻿Console.WriteLine("Ищем максимум");
-Console.WriteLine("Введите 1 число");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите 2 число");
-int b = Convert.ToInt32(Console.ReadLine());
-int max = a;
-if (b > max) max = b;
-Console.Write("Максимум: ");
-Console.WriteLine(max);
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+//Console.WriteLine("Ищем максимум");
+//Console.WriteLine("Введите 1 число");
+//int a = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите 2 число");
+//int b = Convert.ToInt32(Console.ReadLine());
+//int max = a;
+//if (b > max) max = b;
+//Console.Write("Максимум: ");
+//Console.WriteLine(max);
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+Console.WriteLine("Введите число:");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.Write("Четность: ");
+Console.WriteLine(a%2==0);
