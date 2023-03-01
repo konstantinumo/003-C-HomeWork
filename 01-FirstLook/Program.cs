01ee66638e38e53d8e14ebf7964380d9f304ad11
konstantin <konst.geekbrains@mail.ru>
2023-03-01 18:54:27 +0300

    Четные числа от 2 до N

--- 01-FirstLook/Program.cs
+++ 01-FirstLook/Program.cs
@@ -11,7 +11,21 @@
 //Console.WriteLine(max);
 
 //Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+//Console.WriteLine("Введите число:");
+//int a = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Четность: ");
+//Console.WriteLine(a%2==0);
+
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
 Console.WriteLine("Введите число:");
-int a=Convert.ToInt32(Console.ReadLine());
-Console.Write("Четность: ");
-Console.WriteLine(a%2==0);
+int a = Convert.ToInt32(Console.ReadLine());
+int upper = a;
+int current = 2;
+if (a%2==1) upper = a-1;
+while (current < upper + 1)
+{
+Console.Write(current);
+Console.Write(' ');
+current = current + 2;
+}
