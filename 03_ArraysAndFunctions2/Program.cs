f22bbaf0cc10fc6102b152002c9101ad1648f248
konstantin <konst.geekbrains@mail.ru>
2023-03-07 13:33:07 +0300

    Сумма элементов на нечетных позициях

--- 03_ArraysAndFunctions2/Program.cs
+++ 03_ArraysAndFunctions2/Program.cs
@@ -1,5 +1,33 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 
+// int arrSize = 6; //количество трехзначных чисел для генерации и обработки
+// int[] CreateArray()
+// {
+//     int[] arr = new int[arrSize];
+//     for (int i = 0; i < arrSize; i++) arr[i] = new Random().Next(100,1000);
+//     return arr;
+// }
+
+// int[] mass = CreateArray();
+// for (int i = 0; i < arrSize; i++) 
+// {
+// Console.Write(mass[i]);
+// Console.Write(' ');
+// }
+// Console.WriteLine();
+// Console.WriteLine("Из них четные: ");
+
+// for (int i = 0; i < arrSize; i++) 
+// {
+//     if (mass[i]%2==0)
+//     {
+//     Console.Write(mass[i]);
+//     Console.Write(' ');
+//    }
+// }
+
+//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
 int arrSize = 6; //количество трехзначных чисел для генерации и обработки
 int[] CreateArray()
 {
@@ -15,13 +43,17 @@ Console.Write(mass[i]);
 Console.Write(' ');
 }
 Console.WriteLine();
-Console.WriteLine("Из них четные: ");
+Console.WriteLine("Из них на нечетных позициях: ");
 
+int sum = 0;
 for (int i = 0; i < arrSize; i++) 
 {
-    if (mass[i]%2==0)
+    if ((i+1)%2 != 0)
     {
     Console.Write(mass[i]);
     Console.Write(' ');
+    sum += mass[i];
     }
 }
+Console.WriteLine();
+Console.WriteLine("Сумма элементов на нечетных позициях: " + sum);
