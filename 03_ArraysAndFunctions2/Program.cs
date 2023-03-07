cb3b1b93d4ae08a54244c3a24f4c7e0d25b2c8b2
konstantin <konst.geekbrains@mail.ru>
2023-03-07 13:13:46 +0300

    выбираем четные числа из массива

---
+++ 03_ArraysAndFunctions2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int arrSize = 6; //количество трехзначных чисел для генерации и обработки
+int[] CreateArray()
+{
+    int[] arr = new int[arrSize];
+    for (int i = 0; i < arrSize; i++) arr[i] = new Random().Next(100,1000);
+    return arr;
+}
+
+int[] mass = CreateArray();
+for (int i = 0; i < arrSize; i++) 
+{
+Console.Write(mass[i]);
+Console.Write(' ');
+}
+Console.WriteLine();
+Console.WriteLine("Из них четные: ");
+
+for (int i = 0; i < arrSize; i++) 
+{
+    if (mass[i]%2==0)
+    {
+    Console.Write(mass[i]);
+    Console.Write(' ');
+    }
+}
