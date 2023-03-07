c637220a2cdc693d805a7b73240c30653c545fb1
konstantin <konst.geekbrains@mail.ru>
2023-03-07 14:02:48 +0300

    Разница между максимальным и минимальным элементами массива

--- 03_ArraysAndFunctions2/Program.cs
+++ 03_ArraysAndFunctions2/Program.cs
@@ -28,6 +28,38 @@
 
 //Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 
+// int arrSize = 6; //количество трехзначных чисел для генерации и обработки
+// int[] CreateArray()
+// {
+//     int[] arr = new int[arrSize];
+//     for (int i = 0; i < arrSize; i++) arr[i] = new Random().Next(100,1000);
+//     return arr;
+// }
+
+// int[] mass = CreateArray();
+// for (int i = 0; i < arrSize; i++) 
+// {
+// Console.Write(mass[i]);
+// Console.Write(' ');
+// }
+// Console.WriteLine();
+// Console.WriteLine("Из них на нечетных позициях: ");
+
+// int sum = 0;
+// for (int i = 0; i < arrSize; i++) 
+// {
+//     if ((i+1)%2 != 0)
+//     {
+//     Console.Write(mass[i]);
+//     Console.Write(' ');
+//     sum += mass[i];
+//     }
+// }
+// Console.WriteLine();
+// Console.WriteLine("Сумма элементов на нечетных позициях: " + sum);
+
+//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
 int arrSize = 6; //количество трехзначных чисел для генерации и обработки
 int[] CreateArray()
 {
@@ -42,18 +74,15 @@ for (int i = 0; i < arrSize; i++)
 Console.Write(mass[i]);
 Console.Write(' ');
 }
-Console.WriteLine();
-Console.WriteLine("Из них на нечетных позициях: ");
 
-int sum = 0;
-for (int i = 0; i < arrSize; i++) 
+int min = mass[0];
+int max = mass[0];
+for (int i = 1; i < arrSize; i++) 
 {
-    if ((i+1)%2 != 0)
-    {
-    Console.Write(mass[i]);
-    Console.Write(' ');
-    sum += mass[i];
-    }
+    if (mass[i] < min) min = mass[i];
+    if (mass[i] > max) max = mass[i];    
 }
 Console.WriteLine();
-Console.WriteLine("Сумма элементов на нечетных позициях: " + sum);
+Console.WriteLine("Минимальный элемент: " + min);
+Console.WriteLine("Максимальный элемент: " + max);
+Console.WriteLine("Разница между ними: " + (max-min));
