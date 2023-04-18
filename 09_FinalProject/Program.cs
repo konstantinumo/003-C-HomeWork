3c1fa8265fd09a67c88cea8dd257053c2c278a3a
konstantin <konst.geekbrains@mail.ru>
2023-04-18 09:54:00 +0300

    FIXED: вывод всех натуральных чисел в промежутке от N до 1

--- 09_FinalProject/Program.cs
+++ 09_FinalProject/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+double GetNumberFromConsole()
+{
+    Console.WriteLine("Введите положительное число ");
+    double a=Convert.ToDouble(Console.ReadLine());
+    if(a<0) Console.WriteLine("ошибка, отрицательное число:" + a); 
+    return a;
+}
+
+int RoundInput(double a)
+{
+    return Convert.ToInt32(Math.Truncate(a));
+}
+
+int max=RoundInput(GetNumberFromConsole());
+Console.WriteLine("Макс = " + max);
+
+void Descent(int a)
+{
+    if(a>0)
+    {
+        Console.Write(a + " ");
+        Descent(--a);
+    }
+}
+
+Descent(max);
