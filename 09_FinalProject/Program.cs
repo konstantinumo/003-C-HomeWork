cc2dd30c78d37d7c44077616a95fdf1ee8fed5e5
konstantin <konst.geekbrains@mail.ru>
2023-04-18 11:20:02 +0300

    Функция аккермана

--- 09_FinalProject/Program.cs
+++ 09_FinalProject/Program.cs
@@ -35,43 +35,75 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-double GetNumberFromConsole()
+// double GetNumberFromConsole()
+// {
+//     double a=Convert.ToDouble(Console.ReadLine());
+//     return a;
+// }
+
+// int RoundInput(double a)
+// {
+//     return Convert.ToInt32(Math.Truncate(a));
+// }
+
+// Console.WriteLine("Введите первую границу диапазона (положительное число) ");
+// int min=RoundInput(GetNumberFromConsole());
+
+// Console.WriteLine("Введите вторую границу диапазона (положительное число) ");
+// int max=RoundInput(GetNumberFromConsole());
+
+// if(min>max)
+// {
+//     int temp=max;
+//     max=min;
+//     min=temp;
+// }
+
+// Console.WriteLine("Мин = " + min);
+// Console.WriteLine("Макс = " + max);
+
+// int curr=min;
+// int sum=0;
+// if(min>0 && max>0)
+// {
+//     while(curr<=max) 
+//     {
+//     sum=sum+curr;
+//     curr++;    
+//     }
+//     Console.WriteLine("Сумма = " + sum);
+// }
+//     else Console.WriteLine("ошибка, отрицательное число");
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+// функция ack(n, m)
+//    пока n ≠ 0
+//      если m = 0
+//        m:= 1
+//      иначе
+//        m:= ack(n, m - 1)
+//      n:= n - 1
+//    вернуть m + 1
+
+
+int AckFunc(int n, int m)
 {
-    double a=Convert.ToDouble(Console.ReadLine());
-    return a;
-}
-
-int RoundInput(double a)
-{
-    return Convert.ToInt32(Math.Truncate(a));
-}
-
-Console.WriteLine("Введите первую границу диапазона (положительное число) ");
-int min=RoundInput(GetNumberFromConsole());
-
-Console.WriteLine("Введите вторую границу диапазона (положительное число) ");
-int max=RoundInput(GetNumberFromConsole());
-
-if(min>max)
-{
-    int temp=max;
-    max=min;
-    min=temp;
-}
-
-Console.WriteLine("Мин = " + min);
-Console.WriteLine("Макс = " + max);
-
-int curr=min;
-int sum=0;
-if(min>0 && max>0)
-{
-    while(curr<=max) 
+    while(n>0)
     {
-    sum=sum+curr;
-    curr++;    
+        if(m==0) m=1; 
+        else m=AckFunc(n, m - 1);
+        n=n-1;
     }
-    Console.WriteLine("Сумма = " + sum);
+    return m+1;
 }
-    else Console.WriteLine("ошибка, отрицательное число");
 
+Console.WriteLine("Введите 1 целое положительное число ");
+int a=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите 2 целое положительное число ");
+int b=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("A(n,m) = " + AckFunc(a,b));
